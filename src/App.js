36310319e052ce9c23bf22e1e92740bfcbd3b712
Fahima-Begum <fahimabegumb.20it@kongu.edu>
2022-11-04 08:43:22 +0530

    added build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+// import logo from './logo.svg';
 import './App.css';
+// import { useState } from "react";
+// import PlantDetailsForm from './Components/PlantDetailsForm';
 import { Link } from 'react-router-dom';
 
-
 function App() {
+
   return (
     <div className="App">
-<Link to='PlantDetailsForm'>PlantDetailsForm</Link>
+      {/* <header className="App-header"> */}
+        Main Page<br/><br/>
+      {/* </header> */}
+      <Link to='PlantDetailsForm'>PlantDetailsForm</Link>
     </div>
   );
 }
